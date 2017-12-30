752f7343779a181552c8a68be3b51977b07bdf9d
ujjwalbatra2509 <ujjwalbatra2509@gmail.com>
2017-12-30 19:15:28 +0530

    passing parameters as call by value.

---
+++ src/call_by_value.java
@@ -0,0 +1,20 @@
+
+
+public class call_by_value {
+    public static void main(String args[]){
+        Test ob = new Test();
+
+        int a = 15, b = 20;
+
+        System.out.println("a and b before call: " + a + " " + b);
+        ob.meth(a,b);
+        System.out.println("a and b before call: " + a + " " + b);
+    }
+}
+
+class Test {
+    void meth ( int i, int j){
+        i *= 2;
+        j /= 2;
+    }
+}
