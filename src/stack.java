b632766b80dd639f041b45d8ce2747b388d88e9b
ujjwalbatra2509 <ujjwalbatra2509@gmail.com>
2018-01-25 18:56:49 +0530

    returning objects( update 1: correcting else in function print())

--- src/stack.java
+++ src/stack.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class stack {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         int n = 0, item;
         Scanner scan = new Scanner(System.in);
         Stck mystack = new Stck();
@@ -9,7 +9,7 @@ public class stack {
         while (n != 4) {
             System.out.println("Enter 1 to push.\nEnter 2 to pop.\nEnter 3 to print stack.\nEnter 4 to quit.");
             n = scan.nextInt();
-            switch (n){
+            switch (n) {
                 case 1:
                     System.out.println("Enter a number to push.");
                     mystack.push(scan.nextInt());
@@ -18,7 +18,8 @@ public class stack {
                     System.out.println(mystack.pop() + " Has been popped");
                     break;
                 case 3:
-                    mystack.print();;
+                    mystack.print();
+                    ;
                     break;
                 case 4:
                     break;
@@ -28,32 +29,35 @@ public class stack {
 }
 
 class Stck {
-    private int tos, stck [] = new int[10];
+    private int tos, stck[] = new int[10];
 
     //initialise top of stack.
-    Stck(){
+    Stck() {
         tos = -1;
     }
 
     //push an element onto the stack.
-    void push(int n){
-        if (tos == stck.length - 1)  System.out.println("Stack Overflow");
+    void push(int n) {
+        if (tos == stck.length - 1) System.out.println("Stack Overflow");
         else stck[++tos] = n;
 
     }
+
     //poping an element from the stack.
-    int pop(){
-        if (tos < 0){
+    int pop() {
+        if (tos < 0) {
             System.out.println("Underflow");
-           return 0;
-        }
-        else return stck[tos--];
+            return 0;
+        } else return stck[tos--];
     }
+
     //printing contents of the stack.
-    void print(){
+    void print() {
         if (tos == -1) System.out.println("Stack is empty.");
-        for (int i = tos; i >= 0 ; i--){
-            System.out.println(stck[i]);
+        else {
+            for (int i = tos; i >= 0; i--) {
+                System.out.println(stck[i]);
+            }
         }
     }
 }
